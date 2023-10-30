d18ea4306b95ee9c6c71c82649f8da43f05ad69e
Long Hoang <longhoang0304@users.noreply.github.com>
2023-10-30 16:37:13 +0000

    feat: finished algos on strings w2 assignments

--- Coursera/Algorithmic Toolbox/Algorithms on Strings/Week 2/bwt/bwt.cpp
+++ Coursera/Algorithmic Toolbox/Algorithms on Strings/Week 2/bwt/bwt.cpp
@@ -6,13 +6,44 @@
 using std::cin;
 using std::cout;
 using std::endl;
+using std::pair;
 using std::string;
 using std::vector;
 
+typedef pair<int, int> pci;
+
 string BWT(const string& text) {
   string result = "";
+  vector<pci> l;
+  int ts = text.size();
+  for (int i = 1; i <= ts; i++) {
+    l.push_back(pci(i % ts, ts - i));
+  }
 
-  // write your code here
+  sort(l.begin(), l.end(), [text, ts](pci a, pci b) {
+    char ca = text[a.second];
+    char cb = text[b.second];
+
+    if (ca != cb) return ca < cb;
+
+    int ia = (a.second + 1) % ts;
+    int ib = (b.second + 1) % ts;
+
+    ca = text[ia];
+    cb = text[ib];
+
+    while (ca == cb) {
+      ia = (ia + 1) % ts;
+      ib = (ib + 1) % ts;
+      ca = text[ia];
+      cb = text[ib];
+    }
+    return ca < cb;
+  });
+
+  for (auto p : l) {
+    result += text[ts - (p.first % ts) - 1];
+  }
 
   return result;
 }
--- Coursera/Algorithmic Toolbox/Algorithms on Strings/Week 2/bwtinverse/bwtinverse.cpp
+++ Coursera/Algorithmic Toolbox/Algorithms on Strings/Week 2/bwtinverse/bwtinverse.cpp
@@ -1,25 +1,94 @@
+/*
+ * Coursera/Algorithms on Strings/Week 2/Problem 2:
+ * Reconstruct a String from its Burrows–Wheeler Transform
+ * 
+ * Author: Ayran Olckers @ The Geekiest One
+ * Site: http://ayran.dev
+ * Respect Coursera Honor Code
+ * Copyright © 2019. All rights reserved
+ *
+ */
+
 #include <algorithm>
 #include <iostream>
-#include <string>
 #include <vector>
 
-using std::cin;
-using std::cout;
-using std::endl;
-using std::string;
 using std::vector;
+using std::string;
 
-string InverseBWT(const string& bwt) {
-  string text = "";
-
-  // write your code here
-
-  return text;
+inline int getIndexInSymbols(char s)
+{
+    switch (s) {
+    case 'A':
+        return 0;
+    case 'C':
+        return 1;
+    case 'G':
+        return 2;
+    case 'T':
+        return 3;
+    default:
+        return 4;
+    }
 }
 
-int main() {
-  string bwt;
-  cin >> bwt;
-  cout << InverseBWT(bwt) << endl;
-  return 0;
+inline int getIndexInRrightColumn(int *c, char s, int cnt)
+{
+    switch (s) {
+    case 'A':
+        return cnt;
+    case 'C':
+        return c[0] + cnt;
+    case 'G':
+        return c[1] + c[0] + cnt;
+    case 'T':
+        return c[2] + c[1] + c[0] + cnt;
+    default:
+        return 0;
+    }
+}
+
+void inverseBWT(const string& bwt)
+{
+    if (bwt.size() == 1) {
+        std::cout << bwt << '\n';
+        return;
+    }
+
+    int count[5] = {0, 0, 0, 0, 0};
+    vector<int> indexes(bwt.size(), 0);
+
+    auto* iptr = &indexes[0];
+    auto* sptr = bwt.c_str();
+    while (*sptr) {
+        *iptr++ = ++count[getIndexInSymbols(*sptr++)];
+    }
+
+    const auto rsize = bwt.size();
+    char r[rsize + 1];
+    r[rsize] = '\0';
+
+    char s = bwt[0];
+    int j = indexes[0];
+    int k = bwt.size() - 1;
+
+    r[k--] = '$';
+    r[k--] = s;
+
+    for (int i = 2, iters = bwt.size() - i; iters--; ++i) {
+        int indexInRrightColumn = getIndexInRrightColumn(count, s, j);
+        r[k--] = bwt[indexInRrightColumn];
+        j = indexes[indexInRrightColumn];
+        s = r[k + 1];
+    }
+
+    std::cout << r << '\n';
+}
+
+int main()
+{
+    std::string bwt;
+    std::cin >> bwt;
+    inverseBWT(bwt);
+    return 0;
 }
