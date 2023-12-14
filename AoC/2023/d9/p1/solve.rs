c98870ce3f151cc672eb92b420d2d307bb41e2c2
Long Hoang <longhoang0304@users.noreply.github.com>
2023-12-14 14:09:19 +0000

    [AoC-2023][feat]: Finished day 9

---
+++ AoC/2023/d9/p1/solve.rs
@@ -0,0 +1,78 @@
+use std::io;
+
+type Sequence = Box<Vec<i64>>;
+
+fn parse_seq(buf: &str) -> Vec<i64> {
+    let seq: Vec<&str> = buf.split(" ").collect();
+    seq.iter().map(|t| t.parse::<i64>().unwrap()).collect()
+}
+
+fn find_gap(seq: &Sequence) -> (Sequence, bool) {
+    let mut gap: Vec<i64> = Vec::new();
+    let mut is_same = true;
+    let mut prev_v = seq[1];
+
+    gap.push(seq[1] - seq[0]);
+
+    for v in seq.iter().skip(2) {
+        let g = *v - prev_v;
+        let lg = *gap.last().unwrap();
+        
+        if is_same && g != lg { is_same = false }
+        
+        prev_v = *v;
+        gap.push(g);
+    }
+
+    (Box::new(gap), is_same)
+}
+
+fn find_next_value(seq: Vec<i64>) -> i64 {
+    let seq_len = seq.len();
+    
+    if seq_len == 1 { return seq[0]; }
+
+    let mut st: Vec<Sequence> = vec![Box::new(seq)];
+
+    loop {
+        let gap = st.last().unwrap();
+        let (new_gap, is_same) = find_gap(gap);
+
+        st.push(new_gap);
+        if is_same { break }
+    }
+
+    while st.len() > 1 {
+        let gap = st.pop().unwrap();
+        let mut seq = st.pop().unwrap();
+        let last = seq.last_mut().unwrap();
+
+        *last += gap.last().unwrap();
+        
+        st.push(seq);
+    }
+    
+    let next_value = st.last().unwrap().last().unwrap();
+    
+    *next_value
+}
+
+fn main() {
+    let mut buffer = String::new();
+    let mut result = 0;
+
+    loop {
+        io::stdin().read_line(&mut buffer).expect("Failed to read line");
+        if buffer == "\n" || buffer == "\r\n" || buffer.is_empty() {
+            break;
+        }
+        
+        let trimmed = buffer.trim();
+        let seq = parse_seq(trimmed);
+        
+        result += find_next_value(seq);
+        buffer.clear();
+    }
+
+    println!("{result}");
+}
