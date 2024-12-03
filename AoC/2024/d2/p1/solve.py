d71b18e4463a72953d6c8fd97d649ae012be5456
Long Hoang <longhoang0304@users.noreply.github.com>
2024-12-03 11:40:06 +0000

    [AoC-2024]: Finished day 2

---
+++ AoC/2024/d2/p1/solve.py
@@ -0,0 +1,39 @@
+from heapq import *
+
+inp = open("./input", "r").readlines()
+left = []
+right = []
+
+def is_safe(report):
+    diff = report[0] - report[1]
+    sign = 1 if diff > 0 else 0
+    diff = abs(diff)
+
+    if len(report) == 2:
+        return 1 <= diff <= 3
+    
+    if not (1 <= diff <= 3): return False
+
+    for i in range(1, len(report) - 1):
+        diff = report[i] - report[i + 1]
+        nsign = 1 if diff > 0 else 0
+        diff = abs(diff)
+
+        if nsign != sign: return False
+        sign = nsign
+
+        if 1 <= diff <= 3:
+            continue
+        
+        return False
+    
+    return True
+
+safe = 0
+
+for line in inp:
+    report = list(map(int, line.split(' ')))
+    if is_safe(report):
+        safe += 1
+
+print(safe)
---
+++ AoC/2024/d2/p2/solve.py
@@ -0,0 +1,54 @@
+from heapq import *
+
+inp = open("./input", "r").readlines()
+
+
+def is_safe(report):
+    if len(report) <= 1: return True
+
+    diff = report[0] - report[1]
+    sign = 1 if diff > 0 else 0
+    diff = abs(diff)
+
+    if len(report) == 2:
+        return 1 <= diff <= 3
+    
+    if not (1 <= diff <= 3): return False
+
+    for i in range(1, len(report) - 1):
+        diff = report[i] - report[i + 1]
+        nsign = 1 if diff > 0 else 0
+        diff = abs(diff)
+
+        if nsign != sign: return False
+        sign = nsign
+
+        if not (1 <= diff <= 3): return False
+    
+    return True
+
+def try_safe(report):
+    if len(report) <= 2: return True
+
+    diff = report[0] - report[1]
+    sign = 1 if diff > 0 else 0
+    diff = abs(diff)
+
+    for i in range(0, len(report)):
+        if is_safe(report[:i] + report[i + 1:]): return True
+    
+    return False
+
+safe = 0
+
+for line in inp:
+    report = list(map(int, line.split(' ')))
+    if is_safe(report):
+        safe += 1
+        continue
+
+    if try_safe(report):
+        safe += 1
+
+
+print(safe)
